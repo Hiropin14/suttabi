922a471cffc41d76ca17fc5352212da036058c78
Hiropin14 <onepeacenami51@gmail.com>
2020-11-25 23:13:13 +0900

    'aikoのハニーメモリー聞いてみて～'

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,10 +1,12 @@
 class Spot < ApplicationRecord
     belongs_to :user, optional: true
+    validates :image, :about, :address, presence: true
     mount_uploader :image, ImageUploader
     
     default_scope -> { order(created_at: :desc) }
     has_many :likes, dependent: :destroy
     has_many :liked_users, through: :likes, source: :user
     
+
     
 end
